84f600aeb9b252f1ede11fadf50d1d1be2e154d7
ash.karki <ash.karki@uipath.com>
2023-01-16 17:15:51 -0500

    adding codemon

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require('express')
 // express app
 const app = express()
 
+// req:request res:response
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to the app!"})
+})
+
 // listen for request
 app.listen(4000, () => {
-    console.log('Listening on port 4000')
+    console.log('Listening on port 4000!')
 })
