74412575897fa2e8f6fdf05e2f663858b9bd1dbe
ash.karki <ash.karki@uipath.com>
2023-01-16 17:20:34 -0500

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express app
@@ -9,6 +11,6 @@ app.get('/', (req, res) => {
 })
 
 // listen for request
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Listening on port 4000!')
 })
