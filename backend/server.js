395126701c95b42db441d85c32bd75c3bdba0b11
ash.karki <ash.karki@uipath.com>
2023-01-16 17:44:23 -0500

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const express = require('express')
 // express app
 const app = express()
 
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // req:request res:response
 app.get('/', (req, res) => {
     res.json({mssg: "Welcome to the app!"})
@@ -12,5 +18,5 @@ app.get('/', (req, res) => {
 
 // listen for request
 app.listen(process.env.PORT, () => {
-    console.log('Listening on port 4000!')
+    console.log('Listening on port ', process.env.PORT)
 })
