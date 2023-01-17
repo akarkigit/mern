04fd919d9406d206526cbca99fb2dc102f426f44
ash.karki <ash.karki@uipath.com>
2023-01-16 19:05:12 -0500

    added workouts.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes.workouts.js')
 
 // express app
 const app = express()
 
 //middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-// req:request res:response
-app.get('/', (req, res) => {
+// route req:request res:response
+//routes import from routes/workouts.js
+/*app.get('/', (req, res) => {
     res.json({mssg: "Welcome to the app!"})
-})
+})*/
+app.use('/api/workouts', workoutRoutes)
 
 // listen for request
 app.listen(process.env.PORT, () => {
